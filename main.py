40797d0ed1dde8c33af67d7fbc7ca540ca551be9
adeleeeeyyyy <mitsuhaadly@gmail.com>
2024-06-03 00:26:00 +0700

    this is a web implementation

---
+++ main.py
@@ -0,0 +1,74 @@
+from flask import Flask, request, send_file, render_template_string
+from pytube import YouTube
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template_string('''
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>YouTube Downloader</title>
+            <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+            <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+        </head>
+        <body>
+            <div class="container" style="margin-top: 50px;">
+                <h4 class="center-align">YouTube Downloader</h4>
+                <form method="post" action="/download">
+                    <div class="input-field">
+                        <input type="text" name="url" id="url" required>
+                        <label for="url">YouTube URL</label>
+                    </div>
+                    <div class="input-field">
+                        <select name="format" id="format" required>
+                            <option value="" disabled selected>Choose format</option>
+                            <option value="mp4">MP4</option>
+                            <option value="mp3">MP3</option>
+                        </select>
+                        <label for="format">Select Format</label>
+                    </div>
+                    <div class="center-align">
+                        <button type="submit" class="btn waves-effect waves-light">Download</button>
+                    </div>
+                </form>
+            </div>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+            <script>
+                document.addEventListener('DOMContentLoaded', function() {
+                    var elems = document.querySelectorAll('select');
+                    M.FormSelect.init(elems, {});
+                });
+            </script>
+        </body>
+        </html>
+    ''')
+
+@app.route('/download', methods=['POST'])
+def download_video():
+    url = request.form['url']
+    file_format = request.form['format']
+    
+    yt = YouTube(url)
+    if file_format == 'mp4':
+        stream = yt.streams.get_highest_resolution()
+        file_extension = '.mp4'
+    else:
+        stream = yt.streams.filter(only_audio=True).first()
+        file_extension = '.mp3'
+    
+    output_file = stream.download(filename='downloaded_video')
+    base, ext = os.path.splitext(output_file)
+    new_file = base + file_extension
+    os.rename(output_file, new_file)
+    
+    return send_file(new_file, as_attachment=True)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
